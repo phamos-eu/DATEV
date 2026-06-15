11850651290e61aae1cf412fe6265299d2bdaef5
Nasir Khan <work.nasirkhan@gmail.com>
2026-06-15 03:06:06 +0000

    Make canonical datev.gb_datev packages authoritative

--- datev/gb_datev/doctype/__init__.py
+++ datev/gb_datev/doctype/__init__.py
@@ -1,12 +1,1 @@
-"""Compatibility package for legacy DocType imports under ``datev.gb_datev``."""
 
-from importlib import import_module
-
-
-_target = import_module("datev.datev.doctype")
-
-__file__ = _target.__file__
-__path__ = list(_target.__path__)
-
-if __spec__ is not None:
-	__spec__.submodule_search_locations = list(__path__)
